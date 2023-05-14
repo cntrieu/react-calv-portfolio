d605fd8f0828196359cc82cfa85845bc5b7db625
Calvin Trieu <calvin.trieu@hotmail.com>
2023-05-14 00:21:22 -0400

    Styling site using tailwind

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {BsInstagram, BsLinkedin, BsGithub} from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+
+const Footer = () => {
+    return (
+        <footer className='h-20 flex justify-center items-center'>
+            <div id='icons' className='flex justify-center'>
+
+        
+                <a href='https://github.com/cntrieu' className='mx-3 text-4xl hover:scale-105 duration-300'><BsGithub /></a>
+
+                <a href='https://www.linkedin.com/in/calvin-trieu-076a4b212/' className='mx-3 text-4xl hover:scale-105 duration-300'><BsLinkedin /></a>
+
+                <a href='https://www.instagram.com/cntrieu/' className='mx-3 text-4xl hover:scale-105 duration-300'><BsInstagram /></a>
+
+                <a href='mailto:calvin.trieu@hotmail.com' className='mx-3 text-4xl hover:scale-105 duration-300'><AiOutlineMail /></a>
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
